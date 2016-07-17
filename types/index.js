8d2c664c9f1769930d26da883f3812ad0d3778f6
Adam Buczynski <adambuczynski@gmail.com>
2016-07-17 18:27:23 +1200

    Fix types index

--- types/index.js
+++ types/index.js
@@ -3,37 +3,37 @@
 /**
  * Base types
  */
-let Base = require('./types/base');
-let Client = require('./types/client');
-let Internal = require('./types/internal');
-let Reported = require('./types/reported');
-let Server = require('./types/server');
+let Base = require('./base');
+let Client = require('./client');
+let Internal = require('./internal');
+let Reported = require('./reported');
+let Server = require('./server');
 
 /**
  * Auth types
  */
-let NotAuthenticated = require('./types/auth/not-authenticated');
-let NotAuthorized = require('./types/auth/not-authorized');
-let UserPending = require('./types/auth/user-pending');
-let UserSuspended = require('./types/auth/user-suspended');
+let NotAuthenticated = require('./auth/not-authenticated');
+let NotAuthorized = require('./auth/not-authorized');
+let UserPending = require('./auth/user-pending');
+let UserSuspended = require('./auth/user-suspended');
 
 /**
  * Client types
  */
-let BadRequest = require('./types/client/bad-request');
-let Exists = require('./types/client/exists');
-let ExpiredToken = require('./types/client/expired-token');
-let FileTooLarge = require('./types/client/file-too-large');
-let InvalidData = require('./types/client/invalid-data');
-let InvalidToken = require('./types/client/invalid-token');
-let NotFound = require('./types/client/not-found');
-let Validation = require('./types/client/validation');
+let BadRequest = require('./client/bad-request');
+let Exists = require('./client/exists');
+let ExpiredToken = require('./client/expired-token');
+let FileTooLarge = require('./client/file-too-large');
+let InvalidData = require('./client/invalid-data');
+let InvalidToken = require('./client/invalid-token');
+let NotFound = require('./client/not-found');
+let Validation = require('./client/validation');
 
 /**
  * Server types
  */
-let SendMail = require('./types/server/send-mail');
-let ServiceUnavailable = require('./types/server/service-unavailable');
+let SendMail = require('./server/send-mail');
+let ServiceUnavailable = require('./server/service-unavailable');
 
 /**
  * Export
